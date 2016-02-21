6a58b47709f6ba40aac9efd43b37142842c1669e
Joseph Finnegan <joseph.finnegan@tutanota.com>
2016-02-21 16:44:10 +0000

    Adding 4 colours, country found count.

--- hello.py
+++ hello.py
@@ -3,15 +3,6 @@ import csv, random
 
 app = Flask(__name__, static_url_path='/static')
 
-@app.route('/')
-def index():
-    return 'Index Page'
-
-@app.route('/hello/')
-@app.route('/hello/<username>')
-def hello_name(username=None):
-    return render_template('hello.html', name=username)
-
 @app.route('/map/')
 def blog():
     return render_template('map.html')
